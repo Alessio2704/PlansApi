7e4d2c0e2d4e1383286f00fadd228b079d8dff96
Alessio2704 <marcuzzial@gmail.com>
2022-02-03 17:18:07 +0100

    update

--- routes/plans.js
+++ routes/plans.js
@@ -1,29 +1,22 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
-const User = require("../model/User")
+const User = require("../model/User");
 
 router.get("/:id", verify, async (req, res) => {
-
-    try {
-        const userDB = await User.findOne({ _id: req.params.id });
-        res.send(userDB.plans);
-    } catch(error) {
-       res.send({"message":"No personal plans found"});
-    }
+  try {
+    const userDB = await User.findOne({ _id: req.params.id });
+    res.send(userDB.plans);
+  } catch (error) {
+    res.send({ message: "No personal plans found" });
+  }
 });
 
 router.put("/:id", verify, async (req, res) => {
-
-    try {
-        let user = await User.findOne({_id: req.params.id}, (doc, err) => {
-            doc.plans = [req.body];
-            doc.save();
-            res.send(doc);
-        });
-        
-    } catch(error) {
-       res.send({"message":"Error"});
-    }
+  let user = await User.findOne({ _id: req.params.id }, (doc, err) => {
+    doc.plans = [req.body];
+    doc.save();
+    res.send(doc);
+  });
 });
 
 module.exports = router;
