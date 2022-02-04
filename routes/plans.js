a515721dd238be9044813907eeaf8fd4c3356f26
Alessio2704 <marcuzzial@gmail.com>
2022-02-04 23:46:38 +0100

    u

--- routes/plans.js
+++ routes/plans.js
@@ -26,11 +26,6 @@ router.post("/delete/:id", verify, async (req, res) => {
 
     try {
         let user = await User.update({_id:req.params.id},{$pull: {plans: req.body}});
-        try {
-            user.updateOne();
-        } catch(err) {
-            res.send({"message": err});
-        }
         res.send({"message": "ok"});
     } catch(error) {
        res.send({"message":"Error"});
