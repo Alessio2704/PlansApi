760baedafa08108115626305b13054860a29b093
Alessio2704 <marcuzzial@gmail.com>
2022-02-07 00:04:33 +0100

    u

--- routes/stats.js
+++ routes/stats.js
@@ -5,16 +5,24 @@ const User = require("../model/User")
 router.post("/:id", verify, (req, res) => {
 
     const user = User.findOne({_id:req.params.id}, function (err, user) {
-        const plan = user.plans.filter(function (plans) {
-            return plans.planName === req.body.planName;
-        }).pop();
-        const exercise = plan.exercises.filter(function (exercises) {
-            return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay);
-        }).pop();
-        exercise.stats.push(req.body.stats);
-        res.send({"message": "Stats Uploaded"});
-        user.save();
-        
+        try {
+            const plan = user.plans.filter(function (plans) {
+                return plans.planName === req.body.planName;
+            }).pop();
+            try {
+                const exercise = plan.exercises.filter(function (exercises) {
+                    return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay);
+                }).pop();
+                exercise.stats.push(req.body.stats);
+                res.send({"message": "Stats Uploaded"});
+                user.save();
+            } catch(error) {
+                res.send({"message":"Exercise not found"})
+            }
+
+        } catch(err) {
+            res.send({"message":"Plan not found"})
+        }
     });
 });
 
