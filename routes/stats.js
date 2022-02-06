49ccddeab611428503af9ee0508ea7564eada4b2
Alessio2704 <marcuzzial@gmail.com>
2022-02-06 14:23:04 +0100

    u

--- routes/stats.js
+++ routes/stats.js
@@ -2,15 +2,16 @@ const router = require("express").Router();
 const verify = require("./verifyToken");
 const User = require("../model/User")
 
-router.post("/:id", (req, res) => {
+router.post("/:id", verify, (req, res) => {
 
     const user = User.findOne({_id:req.params.id}, function (err, user) {
         const plan = user.plans.filter(function (plans) {
             return plans.planName === req.body.planName;
         }).pop();
-        const exercise = plan.exercises.filter(function (exercise) {
-            return exercise.name === req.body.exerciseName;
+        const exercise = plan.exercises.filter(function (exercises) {
+            return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay)
         }).pop();
+        exercise.stats.push(req.body.stats)
         res.send(exercise);
         user.save();
     });
