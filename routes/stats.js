fea121df3c5d59b1be7af70604280cead33f3e5e
Alessio2704 <marcuzzial@gmail.com>
2022-02-07 14:03:08 +0100

    u

--- routes/stats.js
+++ routes/stats.js
@@ -26,4 +26,27 @@ router.post("/:id", verify, (req, res) => {
     });
 });
 
+router.put("/:id", verify, (req, res) => {
+
+    const user = User.findOne({_id:req.params.id}, function (err, user) {
+        try {
+            const plan = user.plans.filter(function (plans) {
+                return plans.planName === req.body.planName;
+            }).pop();
+            try {
+                const exercise = plan.exercises.filter(function (exercises) {
+                    return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay);
+                }).pop();
+                res.send(exercise.stats);
+                user.save();
+            } catch(error) {
+                res.send({"message":"Exercise not found"})
+            }
+
+        } catch(err) {
+            res.send({"message":"Plan not found"})
+        }
+    });
+});
+
 module.exports = router;
