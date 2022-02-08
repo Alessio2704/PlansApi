4411b59225563ba3f24d222e180d9be4587d25a3
Alessio2704 <marcuzzial@gmail.com>
2022-02-08 14:23:31 +0100

    u

--- routes/stats.js
+++ routes/stats.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
-const User = require("../model/User")
+const User = require("../model/User");
+const Coach = require("../model/Coach");
 
 router.post("/:id", verify, (req, res) => {
 
@@ -39,6 +40,53 @@ router.put("/:id", verify, (req, res) => {
                 }).pop();
                 res.send(exercise.stats);
                 user.save();
+            } catch(error) {
+                res.send({"message":"Exercise not found"})
+            }
+
+        } catch(err) {
+            res.send({"message":"Plan not found"})
+        }
+    });
+});
+
+router.post("/coach/:id", verify, (req, res) => {
+
+    const coach = Coach.findOne({_id:req.params.id}, function (err, coach) {
+        try {
+            const plan = coach.plans.filter(function (plans) {
+                return plans.planName === req.body.planName;
+            }).pop();
+            try {
+                const exercise = plan.exercises.filter(function (exercises) {
+                    return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay);
+                }).pop();
+                exercise.stats.push(req.body.stats);
+                res.send({"message": "Stats Uploaded"});
+                coach.save();
+            } catch(error) {
+                res.send({"message":"Exercise not found"})
+            }
+
+        } catch(err) {
+            res.send({"message":"Plan not found"})
+        }
+    });
+});
+
+router.put("/coach/:id", verify, (req, res) => {
+
+    const coach = Coach.findOne({_id:req.params.id}, function (err, coach) {
+        try {
+            const plan = coach.plans.filter(function (plans) {
+                return plans.planName === req.body.planName;
+            }).pop();
+            try {
+                const exercise = plan.exercises.filter(function (exercises) {
+                    return (exercises.name === req.body.exerciseName && exercises.day === req.body.exerciseDay);
+                }).pop();
+                res.send(exercise.stats);
+                coach.save();
             } catch(error) {
                 res.send({"message":"Exercise not found"})
             }
